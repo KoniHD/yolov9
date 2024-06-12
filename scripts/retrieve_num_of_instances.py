5b82b45e3547fa657f5593b2279181dd79d60213
Koni <konstantin.zeck@gmail.com>
2024-06-12 11:13:20 +0200

    changed parameter naming from fileId to classId to avoid confusion

--- scripts/retrieve_num_of_instances.py
+++ scripts/retrieve_num_of_instances.py
@@ -10,7 +10,7 @@ Parameters
 ----------
 DIR : str
     Directory path to label folder. It might contain the subdirectories `train`, `val` and `test` 
-fileID: int
+classID: int
     Id of the class to be searched. Possible Ids are 0 (small load carrier), 1 (forklift), \
         2 (pallet), 3 (stillage), 4 (pallet truck)
 """
@@ -45,8 +45,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     # for use in GitHub Codespaces use '/workspaces' as directory
     # default is the google colab directory
-    parser.add_argument('--DIR', '--dir', '-d', type=str, help='Directory path to label folder', default='/workspace/yolov9/loco')
-    parser.add_argument('--fileId', '--id', '-fi', type=int, help='ID of the class to be searched')
+    parser.add_argument('--DIR', '--dir', '-d', type=str, help='Directory path to label folder', default='/content/yolov9/loco')
+    parser.add_argument('--classId', '-id', '-ci', type=int, choices=[0, 1, 2, 3, 4], help='ID of the class to be searched')
     args = parser.parse_args()
     return args
 
@@ -55,15 +55,13 @@ if __name__ == "__main__":
 
     if not os.path.exists(arg.DIR):
         raise FileNotFoundError(f"Directory {arg.DIR} does not exist")
-    if arg.fileId not in class_names.keys():
-        raise ValueError(f"Class ID {arg.fileId} is not valid. Valid IDs are {class_names.keys()}")
     
     if os.path.exists(os.path.join(arg.DIR, 'train')):
-        numOfInstances, listOfFiles = get_filenames_of_id(arg.fileId, os.path.abspath(arg.DIR), 'train')
-        print(f"There are {numOfInstances} images of category {arg.fileId} in the train dataset. Located in the file InstancesIn_train.txt.")
+        numOfInstances, listOfFiles = get_filenames_of_id(arg.classId, os.path.abspath(arg.DIR), 'train')
+        print(f"There are {numOfInstances} images of category {arg.classId} in the train dataset. Located in the file InstancesIn_train.txt.")
     if os.path.exists(os.path.join(arg.DIR, 'val')):
-        numOfInstances, listOfFiles = get_filenames_of_id(arg.fileId, os.path.abspath(arg.DIR), 'val')
-        print(f"There are {numOfInstances} images of category {arg.fileId} in the val dataset. Located in the file InstancesIn_val.txt.")
+        numOfInstances, listOfFiles = get_filenames_of_id(arg.classId, os.path.abspath(arg.DIR), 'val')
+        print(f"There are {numOfInstances} images of category {arg.classId} in the val dataset. Located in the file InstancesIn_val.txt.")
     if os.path.exists(os.path.join(arg.DIR, 'test')):
-        numOfInstances, listOfFiles = get_filenames_of_id(arg.fileId, os.path.abspath(arg.DIR), 'test')
-        print(f"There are {numOfInstances} images of category {arg.fileId} in the test dataset. Located in the file InstancesIn_test.txt.")
+        numOfInstances, listOfFiles = get_filenames_of_id(arg.classId, os.path.abspath(arg.DIR), 'test')
+        print(f"There are {numOfInstances} images of category {arg.classId} in the test dataset. Located in the file InstancesIn_test.txt.")
