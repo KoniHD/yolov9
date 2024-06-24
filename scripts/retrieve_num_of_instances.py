2bbf1ca05bde5176580b786c5c57869c9e0bcfc3
KoniHD <konstantin.zeck+github@gmail.com>
2024-06-24 21:37:32 +0000

    Improving functionallity for multiple class ids to be searched

--- scripts/retrieve_num_of_instances.py
+++ scripts/retrieve_num_of_instances.py
@@ -1,5 +1,5 @@
 """
-This script is intended to get picture ids containing a given id from the LOCO dataset.
+This script is intended to get picture ids containing a given category id from the LOCO dataset.
 Potential Ids are:
   0: small_load_carrier
   1: forklift
@@ -18,35 +18,45 @@ classID: int
 import os
 import argparse
 
-class_names = {
-    0: 'small load carrier',
-    1: 'forklift',
-    2: 'pallet',
-    3: 'stillage',
-    4: 'pallet truck'
-}
-
-def get_filenames_of_id(id, dir, subdir):
+def get_filenames_of_id(dir, subdir, classId):
+    numOfInstances = 0
     file_list = []
+    # create file to store the list of files containing the classId
     with open (f"InstancesIn_{subdir}.txt", 'w') as listOfFiles:
         for file in os.listdir(os.path.join(dir, subdir)):
-            with open(os.path.join(dir, subdir, file), 'r') as f:
-                for line in f:
-                    class_id = int(line.split()[0])
-                    if class_id == id:
-                        filename, _ = os.path.splitext(file)
-                        file_list.append(filename + '.jpg')
-                        listOfFiles.write(f"{filename}.jpg\n")
-                        break
-    return len(file_list), listOfFiles
+            contains_id = False
 
+            # open file and check if classId is in the file
+            with open(os.path.join(dir, subdir, file), 'r') as f:
+                if classId is None:
+                    numOfInstances += sum(1 for line in f)
+                    filename, _ = os.path.splitext(file)
+                    file_list.append(filename + '.jpg')
+                else:
+                    for line in f:
+                        data = line.strip().split()
+                        if int(data[0]) in classId:
+                            numOfInstances += 1
+                            if not contains_id:
+                                filename, _ = os.path.splitext(file)
+                                file_list.append(filename + '.jpg')
+                                listOfFiles.write(f"{filename}.jpg\n")
+                                contains_id = True
+    if classId is None:
+        os.remove(f"InstancesIn_{subdir}.txt")
+
+    return len(file_list), numOfInstances
+
+def to_set(arg):
+    return set(map(lambda x: int(x.strip()), arg.split(',')))
 
 def parse_args():
     parser = argparse.ArgumentParser()
     # for use in GitHub Codespaces use '/workspaces' as directory
     # default is the google colab directory
     parser.add_argument('--DIR', '--dir', '-d', type=str, help='Directory path to label folder', default='/content/yolov9/loco')
-    parser.add_argument('--classId', '-id', '-ci', type=int, choices=[0, 1, 2, 3, 4], help='ID of the class to be searched')
+    parser.add_argument('--classId', '--id', '-ci', type=to_set, help='Comma-seperated list of Class IDs to be searched. \
+                        If none is given all classes will be displayed.')
     args = parser.parse_args()
     return args
 
@@ -55,13 +65,33 @@ if __name__ == "__main__":
 
     if not os.path.exists(arg.DIR):
         raise FileNotFoundError(f"Directory {arg.DIR} does not exist")
+    if arg.classId and not all(isinstance(i, int) and 0<=i and i<=4 for i in arg.classId):
+        raise ValueError(f"Class ID {arg.classId} is not valid. Valid IDs are 0, 1, 2, 3, 4.")
+    
+    numOfInstances = 0
     
     if os.path.exists(os.path.join(arg.DIR, 'train')):
-        numOfInstances, listOfFiles = get_filenames_of_id(arg.classId, os.path.abspath(arg.DIR), 'train')
-        print(f"There are {numOfInstances} images of category {arg.classId} in the train dataset. Located in the file InstancesIn_train.txt.")
+        numOfImages, numOfInstancesTrain = get_filenames_of_id(os.path.abspath(arg.DIR), 'train', arg.classId)
+        numOfInstances += numOfInstancesTrain
+        if arg.classId is not None:
+            print(f"There are {numOfImages} images containing an object of category {arg.classId} in the train dataset." +
+                  " The image_files are located in the file InstancesIn_train.txt.")
+        else:
+            print(f"There are {numOfImages} images in the train dataset containing an annotated object.")
     if os.path.exists(os.path.join(arg.DIR, 'val')):
-        numOfInstances, listOfFiles = get_filenames_of_id(arg.classId, os.path.abspath(arg.DIR), 'val')
-        print(f"There are {numOfInstances} images of category {arg.classId} in the val dataset. Located in the file InstancesIn_val.txt.")
+        numOfImages, numOfInstancesVal = get_filenames_of_id(os.path.abspath(arg.DIR), 'val', arg.classId)
+        numOfInstances += numOfInstancesVal
+        if arg.classId is not None:
+            print(f"There are {numOfImages} images containing an object of category {arg.classId} in the train dataset." +
+                  " The image_files are located in the file InstancesIn_train.txt.")
+        else:
+            print(f"There are {numOfImages} images in the train dataset containing an annotated object.")
     if os.path.exists(os.path.join(arg.DIR, 'test')):
-        numOfInstances, listOfFiles = get_filenames_of_id(arg.classId, os.path.abspath(arg.DIR), 'test')
-        print(f"There are {numOfInstances} images of category {arg.classId} in the test dataset. Located in the file InstancesIn_test.txt.")
+        numOfImages, numOfInstancesTest = get_filenames_of_id(os.path.abspath(arg.DIR), 'test', arg.classId)
+        numOfInstances += numOfInstancesTest
+        if arg.classId is not None:
+            print(f"There are {numOfImages} images containing an object of category {arg.classId} in the train dataset." +
+                  " The image_files are located in the file InstancesIn_train.txt.")
+        else:
+            print(f"There are {numOfImages} images in the train dataset containing an annotated object.")
+    print(f"Total number of images of category {arg.classId} is {numOfInstances}")
