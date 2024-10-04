6fcec5664f6655ec7e2b1ea24844a712c901ead1
Koni <konstantin.zeck+github@gmail.com>
2024-10-04 10:49:17 +0200

    Trying to see if yolov9pip strucutre works

--- yolov9/__init__.py
+++ yolov9/__init__.py
@@ -1,9 +1,11 @@
-from .models.common import DetectMultiBackend
-from .utils.general import non_max_suppression, scale_boxes
-from .utils.torch_utils import select_device, smart_inference_mode
-from .utils.augmentations import letterbox
+# from .models.common import DetectMultiBackend
+# from .utils.general import non_max_suppression, scale_boxes
+# from .utils.torch_utils import select_device, smart_inference_mode
+# from .utils.augmentations import letterbox
 
-__all__ = [
-    'DetectMultiBackend', 'non_max_suppression', 'scale_boxes',
-    'select_device', 'smart_inference_mode', 'letterbox'
-]
+# __all__ = [
+#     'DetectMultiBackend', 'non_max_suppression', 'scale_boxes',
+#     'select_device', 'smart_inference_mode', 'letterbox'
+# ]
+
+from yolov9.helpers import load_model as load
